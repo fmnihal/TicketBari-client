207cde6405ec2a21f1f3147dd7201f38539ada62
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-10 16:12:02 +0600

    react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import MainLayout from '../src/layout/MainLayout';
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: MainLayout,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </StrictMode>,
 )
