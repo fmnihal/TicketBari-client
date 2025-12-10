22a315db328c60c029f82269444bdfb64e36ee41
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-10 16:37:28 +0600

    Auth Layout

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import Home from '../pages/Home';
 import AllTickets from '../pages/AllTickets';
+import AuthLayout from "../layouts/AuthLayout";
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +13,14 @@ export const router = createBrowserRouter([
     children: [
         {index: true, Component: Home},
         {path:'all-tickets', Component: AllTickets},
+    ]
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+        {path: 'login', Component: Login},
+        {path: 'register', Component: Register},
     ]
   }
 ]);
