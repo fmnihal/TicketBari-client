a651af76cddf5798b5e88056fe56bd541f9b7326
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 22:45:35 +0600

    special offers

---
+++ src/components/SpecialOffers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SpecialOffers = () => {
+    return (
+        <div className='h-96 border-2 border-amber-950 mt-10'>
+            <h1 className='text-3xl'>Special Offers</h1>
+        </div>
+    );
+};
+
+export default SpecialOffers;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 // import banner from '../../public/banner.png';
 import Banner from '../components/Banner';
+import SpecialOffers from '../components/SpecialOffers';
 
 const Home = () => {
     return (
-        <div>
+        <div className='mx-15'>
             {/* <img src={banner} alt="" className='mx-auto h-[500px]' /> */}
             <Banner></Banner>
+            <SpecialOffers></SpecialOffers>
         </div>
     );
 };
