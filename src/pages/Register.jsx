644d986f2494fa6da587eee05386df1b0214c841
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 23:20:20 +0600

    register fix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            Register
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <legend className="fieldset-legend">Register</legend>
+
+            <label className="label">Name</label>
+            <input type="text" className="input" placeholder="Enter your name" />
+
+            <label className="label">Email</label>
+            <input type="text" className="input" placeholder="enter your email" />
+
+            <label className="label">Password</label>
+            <input type="text" className="input" placeholder="enter your password" />
+            <label className="label">Confirm Password</label>
+            <input type="text" className="input" placeholder="confirm your password" />
+            </fieldset>
         </div>
     );
 };
