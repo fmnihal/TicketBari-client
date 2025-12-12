e76c4d88c9a6e164248f94af9f354bab51b93896
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 23:08:01 +0600

    card size fix

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TicketCard = () => {
     return (
-        <div className='h-16 w-16 border-2 border-amber-950 mt-10'>
+        <div className='h-60 w-60 border-2 border-amber-950 mt-10'>
             <h1 className='text-3xl'>Ticket Card</h1>
         </div>
     );
