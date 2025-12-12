3551a0f4dc7cdb2218a0d70870462d794fff02b6
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 23:06:29 +0600

    card

---
+++ src/components/TicketCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TicketCard = () => {
+    return (
+        <div className='h-16 w-16 border-2 border-amber-950 mt-10'>
+            <h1 className='text-3xl'>Ticket Card</h1>
+        </div>
+    );
+};
+
+export default TicketCard;
--- src/pages/AllTickets.jsx
+++ src/pages/AllTickets.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SearchBox from '../components/SearchBox';
+import TicketCard from '../components/TicketCard';
 
 const AllTickets = () => {
     return (
         <div>
             <h1>Explore all Available Tickets</h1>
+            <SearchBox></SearchBox>
+            <TicketCard></TicketCard>
         </div>
     );
 };
