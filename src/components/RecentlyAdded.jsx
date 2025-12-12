bc21e96cba5ce9071017ab7a59d2866a0fa53444
fmnihal <fattahmahmudnihal@gmail.com>
2025-12-12 22:57:59 +0600

    recently added

---
+++ src/components/RecentlyAdded.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentlyAdded = () => {
+    return (
+        <div className='h-96 border-2 border-amber-950 mt-10'>
+            <h1 className='text-3xl'>Recently Added</h1>
+        </div>
+    );
+};
+
+export default RecentlyAdded;
